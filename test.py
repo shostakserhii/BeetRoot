56dcd6d02e2a6e340a711941275367e546e607d0
shostakserhii <62908234+shostakserhii@users.noreply.github.com>
2020-04-02 20:33:40 +0300

    Update test.py

---
+++ test.py
@@ -0,0 +1,73 @@
+# 1st comment
+int_value = 1 # ціле число int -infinity to +infinity
+negative_int_value = -1 #-infinity
+print(int_value)
+print()
+print(negative_int_value)
+print()
+float_value = 1.234 # Float дробове число або число з плаваючою точкою
+print(float_value)
+print()
+string_value = 'Text' # string/str стрічка with quotes
+print(string_value)
+print()
+string_second = "Text 2" # Str with double quotes
+print()
+meaningful_sentence = "once upon a time ... don't"
+print(meaningful_sentence)
+print()
+meaningful_sentence_two = 'once upon a time ... don\'t \\m/'
+print(meaningful_sentence_two)
+print()
+meaningful_sentence_two = 'once upon a time ... don\'t \\m/'
+print(meaningful_sentence_two)
+print()
+meaningful_sentence_three = 'First line \n\tSecond line'
+print(meaningful_sentence_three)
+print()
+very_long_str = 'Very long long long sentence without any sence or context but' +\
+'still it is very long'
+print(very_long_str)
+print()
+paragraph_text = """First line
+Second line
+Third line"""
+print(paragraph_text)
+print()
+
+negative_statemnt = False #False actually is 0
+positive_statemnt = True #True actually is 1
+
+print(negative_statemnt)
+print(positive_statemnt)
+
+print()
+
+#calculations
+x = 1
+y = 2
+print(x + y)
+print(x - y)
+print(x / y)
+print(x * y)
+print(x % y)
+print()
+print(f'Sum: {x} + {y} = {x + y}')
+
+print(f'Ext: {x} - {y} = {x - y}')
+
+print(f'Div: {x} / {y} = {x / y}')
+
+print(f'Mult: {x} * {y} = {x * y}')
+
+print(f'Modul: {x} % {y} = {x % y}')
+print()
+print(f'Text: {paragraph_text}')
+print()
+x = 2
+y = 3
+comparison_result = x == y
+
+print(f'Comparing {x} and {y}: {comparison_result}')
+
+print(f'Is {x} less than {y}: {x<y}')
