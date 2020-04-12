56c683b247f2b70eb7de196ff8df8ce8fa4415a2
shostakserhii <62908234+shostakserhii@users.noreply.github.com>
2020-04-12 18:02:36 +0300

    Homework done

---
+++ Home#1_First_steps.py
@@ -0,0 +1,23 @@
+#2
+#Create a python program named “task2”, and use the built-in function `print` in it several times
+# Try to pass “sep”, “end” params and pass several parameters separated by commas. 
+#Also, provide a comment text above each print statement, mentioned above,
+#with the expected output after execution of the particular print statement.
+print("hello world")
+print()
+int_value = 1 
+print ("int_value= ",int_value)
+print()
+print('We are separating one sting in 2 stings:')
+print("This is first line\nthis is second one") 
+print()
+print("Виводимо одну тисячну через 'sep':")
+print("1/1000 = ",'1',sep='0,00')
+print()
+print("Виводимо одну тисячну в трьох окремих рядках via '\\n':")
+print("1/1000\n=\n",'1\n',sep='0,00')
+print()
+print("Tast#3: print 'O' and 'H':")
+print("#########\n#\t\t#\n#\t\t#\n#\t\t#\n#########")
+print()
+print("#\t\t#\n#\t\t#\n#########\n#\t\t#\n#\t\t#")
