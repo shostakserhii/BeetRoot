8400690eb558d786af7741e24ef85e4a734ebefa
shostakserhii <62908234+shostakserhii@users.noreply.github.com>
2020-05-14 00:35:32 +0300

    Update Calculator_automode.py

--- Calculator/Calculator_automode.py
+++ Calculator/Calculator_automode.py
@@ -1,21 +1,14 @@
-def command_validation(command):
-    operation = get_operation(command)
-    if operation is None:
-        return None   
-    left, symbol, right = command.partition(operation)
-    if validation_num(left) and validation_num(right): 
-        for symbol in operations_auto.keys():
-            if operation == symbol: 
-                if operation == "/" or operation == '//' or operation =='%':
-                    if right == 0:
-                        return print(f"Sorry, {name}, but you cannot divide by 0")
-                    else:
-                        left = float(left)
-                        right = float(right)
-                        return operations_auto[symbol](left,right)
-                left = float(left)
-                right = float(right)
- #               if operations_auto[symbol](left,right).isinstance():
- #                   return int(operations_auto[symbol](left,right))
-                return operations_auto[symbol](left,right) 
-    return None
+from operator import pow, truediv, mul, add, sub  
+x = ("-2--2")
+l=list()
+def minus(line):
+    i = 0
+    n=0
+    x = list()
+    y= list()
+    while True:
+        if i < len(line):
+            if line[i]=='-' and [i]==0:
+                x.append(line[i])
+                print(x)
+minus(x)
