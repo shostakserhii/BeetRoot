43f9360fd100bbfc5301a3a1471312a5a2798f8f
shostakserhii <62908234+shostakserhii@users.noreply.github.com>
2020-05-19 23:09:46 +0300

    added

--- Home#9_Files/PB.py
+++ Home#9_Files/PB.py
@@ -9,11 +9,24 @@ class User():
         self.city = city
         self.full_name = self.first_name+" "+self.second_name
 
-#    def __str__(self):
-#        return print(f'first name: {self.first_name} second_name: {self.second_name} phone: {self.phone} city {self.city} fullname: {self.full_name}')
-#
-#    def __repr__(self):
-#       return print(f'first name: {self.first_name} second_name: {self.second_name} phone: {self.phone} city {self.city} fullname: {self.full_name}')
+
+    def __str__(self):
+        return f"""
+                First name: {self.first_name} 
+                Second_name: {self.second_name} 
+                Phone: {self.phone}
+                City {self.city} 
+                Full name: {self.full_name}"""
+
+
+    def __repr__(self):
+        return f"""
+                First name: {self.first_name} 
+                Second_name: {self.second_name} 
+                Phone: {self.phone}
+                City {self.city} 
+                Full name: {self.full_name}"""
+
 
     def convert(self):
         return {
@@ -23,16 +36,57 @@ class User():
             'city':self.city,
             'full_name':self.first_name+" "+self.second_name
         }
+    
+##################################################
+#           VARIABLES                            #
+##################################################
+
+first_name = 'first name'
+second_name = 'second name'
+full_name = 'full name'
+phone = 'phone'
+city= 'city'
+
+validation_listing = [first_name, second_name, city, full_name]
+
+####################################################
+#               Validations                        #
+####################################################
+
+def validation_num(phone):
+    if phone.isdigit():
+        return phone
+    print('Incorrect input. Try again')
+
+
+def validation_name(name):
+    if name.isalpha():
+        return name
+    print('Incorrect input. Try again')
+
+
+def validation(inputing, category):
+    if str(category) == 'phone':
+        if inputing.isdigit():
+            return inputing
+        return None
+    elif category in validation_listing: 
+        if inputing.isalpha():
+            return inputing
+    print('Incorrect input. Try again')
+    return None
+
+
 ####################################################
 #               BODY FUNCTIONS                     #
 ####################################################
 
 def new_user():
-    first_name = input("Enter first name: ")
-    second_name = input("Enter second name: ")
+    first_name = input("Enter first name: ").capitalize()
+    second_name = input("Enter second name: ").capitalize()
     phone = input("Enter phone: ")
-    city = input("Enter city: ")
-    full_name = (first_name + "" + second_name)
+    city = input("Enter city: ").capitalize()
+    full_name = (first_name + '' + second_name)
     new_user = User(first_name, second_name, phone, city , full_name)
     phonebook.append(new_user)
 
@@ -40,21 +94,23 @@ def new_user():
 def delete(phone):
     for item in phonebook:
         if item.phone==phone:
-            phonebook.remove(item)
             print(f"Successfuly removed {item}")
+            del item
+            
 
 
 def update(phone):
     for item in phonebook:
         if item.phone==phone:
-            item.first_name = input(f"First name is {item.first_name}. Enter name you want it to be changed to: ")
-            item.second_name = input(f"Second name is {item.second_name}. Enter name you want it to be changed to: ")
+            item.first_name = input(f"First name is {item.first_name}. Enter name you want it to be changed to: ").capitalize()
+            item.second_name = input(f"Second name is {item.second_name}. Enter name you want it to be changed to: ").capitalize()
             item.full_name = item.first_name.capitalize() + " " + item.second_name.capitalize()
-            item.city = input(f"City is {item.city}. Enter name you want it to be changed to: ")
+            item.city = input(f"City is {item.city}. Enter name you want it to be changed to: ").capitalize()
 
 
 def what_to_do(item):
-    choice = input("""What do you want to do with abonent:
+    choice = input("""
+        What do you want to do with abonent:
                     d - delete
                     u - update
                     c - continue seraching
@@ -63,15 +119,30 @@ def what_to_do(item):
         phonebook.remove(item)
         return print("Abonent is erased")
     elif choice == 'u':
-            item.first_name = input(f"First name is {item.first_name}. Enter new first name: ")
-            item.second_name = input(f"Second name is {item.second_name}. Enter new second name: ")
+            item.first_name = input(f"First name is {item.first_name}. Enter new first name: ").capitalize()
+            item.second_name = input(f"Second name is {item.second_name}. Enter new second name: ").capitalize()
             item.full_name = item.first_name.capitalize() + " " + item.second_name.capitalize()
-            item.city = input(f"City is {item.city}. Enter new city: ")
+            item.city = input(f"City is {item.city}. Enter new city: ").capitalize()
             item.phone = input(f"Phone is: {item.phone}. Enter new phone: ")
             return print("Updated")
     elif choice == 'c':
         return None
 
+
+def search(operation):
+    value = validation(input(f"Enter {str(operation)} you want to find: "),operation)
+    if value is None:
+        return print("wrong input") 
+    else:
+        i = 0
+        for item in phonebook:
+            if getattr(item, operation) == value:
+                i+=1
+                print(f"\nAbonent #{i}\n")
+                print(item)
+                what_to_do(item)
+        if i == 0:
+            return print("No results found")
 ##############################################
 #                     MAIN BODY              #
 ##############################################
@@ -111,15 +182,15 @@ try:
             """)
             choice = input("Choose operation: ")
             if choice.strip().lower() == "f":
-                search_first()
+                search('first name')
             elif choice.strip().lower() == "s":
-                search_second()
+                search('second name')
             elif choice.strip().lower() == "n":
-                search_full()
+                search('full name')
             elif choice.strip().lower() == "t":
-                search_tel()
+                search(phone)
             elif choice.strip().lower() == "c":
-                search_city()
+                search("city")
             else:
                 print("Not available operation")
         elif choice.strip().lower() == "d":
@@ -131,7 +202,7 @@ try:
         else:
             print("Incorrect input")
             continue
-except ValueError as e:
+except Exception as e:
     print(f"Error: {e} happened but data is saved, all ok! ")
 finally:
     phonebook = [new_user.convert() for new_user in phonebook]
