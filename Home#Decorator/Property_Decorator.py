4852b31a6d7caa4749b7a1f317efd84f9223a85b
shostakserhii <62908234+shostakserhii@users.noreply.github.com>
2020-06-07 15:59:08 +0300

    Update Property_Decorator.py

--- Home#Decorator/Property_Decorator.py
+++ Home#Decorator/Property_Decorator.py
@@ -1,20 +1,73 @@
-class Admin_User():
+class Test:
+    def __init__(self, var):
+        self.__var = var
+    
+    def get_var(self):
+        print("get")
+        return self.__var
+    
+    def set_var(self, var):
+        print("set")
+        self.__var = var
 
-    def __init__(self, password = 'q'):
-        self.password = password
+    our_property = property(get_var, set_var)
+    
+t = Test(1)
+print(t.our_property)
+t.our_property = 10
+print(t.our_property)
 
+class Test2:
+    def __init__(self, var):
+        self.__var = var
+    
     @property
-    def password(self):
-        print("getting....")
-        return self.__password
+    def get_var(self):
+        print("get")
+        return self.__var
+    @get_var.setter
+    def set_var(self, var):
+        print("set")
+        self.__var = var
 
-    @property
-    def password(self, password):
-        old = input("old password: ")
-        if old == self.password:
-            self.__password = password
-        else: raise ValueError("wrong pass")
+    
+x = Test2(5)
+print(x.get_var)
+x.set_var = 10
+print(x.get_var)
+print()
+print()
+print("Static Method/Class Method")
 
 
-user = Admin_User('s')
-print(user.password)
+class Stat_test():
+
+    def __init__(self, x = "STAT_TEST"):
+        self.x = x
+    
+    @staticmethod
+    def static_method(value):
+        if type(value) is str: 
+            return f"{value.lower()}"
+        raise ValueError("should be str")
+
+    @classmethod
+    def class_method(cls,x):
+        return cls(x.lower())
+    
+    def __str__(self):
+        return self.x
+    def __repr__(self):
+        return self.x
+
+
+
+st = Stat_test
+print(st.static_method("UPPPER"))
+
+val = ("LOWER")
+test  = Stat_test.class_method(val)
+print(test)
+
+
+
