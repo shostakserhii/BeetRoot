1826e2b4da25cfb22a3320f2b5b343dad69eb232
shostakserhii <62908234+shostakserhii@users.noreply.github.com>
2020-06-10 22:54:58 +0300

    Done

--- Home#Generators_Iterators/Task1.py
+++ Home#Generators_Iterators/Task1.py
@@ -2,16 +2,15 @@ import itertools
 
 def with_index(iterable, start = 0):
     x = list(zip(itertools.count(start), iterable))
-    for i in x:
-        print(i)
+    return x
         
 def with_index2(iterable, start = 0):
+
     while start<len(iterable):
-        print(f"#{start}, element: {iterable[start]}, {type(iterable[start])}")
+        yield (start, iterable[start])
         start += 1
 
 x = [1, 3, 5, 'a', '10', 3]
 
-
-with_index(x)
-with_index2(x, 2)
+for i in with_index2(x, 2):
+    print(i)
--- Home#Generators_Iterators/Task2.py
+++ Home#Generators_Iterators/Task2.py
@@ -21,24 +21,22 @@ x = in_range(0, -100, -5)
 print(x)
 """
 
-def in_range(start, end, step = 1):
-
+def in_range(start, end = None, step = 1):
+    if end == None:
+        end = start
+        start = 0
     if (start > end and step > 0):
-        return ("start should be < end if step > 0")
+        return []
     if (start < end and step < 0):
-        return ("start should be > end if step < 0")
+        return []
 
     while start != end:
-
-        if (step > 0 and start>end):
-            break
-        if (step < 0 and start<end):
-            break
 
         yield start
 
         start += step
 
 
-x = list(in_range(10, 0, 1))
+x = list(in_range(0, 10 ,1))
 print(x)
+
