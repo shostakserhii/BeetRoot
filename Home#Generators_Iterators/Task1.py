ebb9c3ac74671439599d594c7858bd0164886a91
shostakserhii <62908234+shostakserhii@users.noreply.github.com>
2020-06-11 14:40:45 +0300

    Update Task1.py

--- Home#Generators_Iterators/Task1.py
+++ Home#Generators_Iterators/Task1.py
@@ -9,13 +9,25 @@ def with_index2(iterable, start = 0):
 
     i=0
 
-    while i<len(list(iterable)):
-
-        yield (start, iterable[i])
-        start += 1
-        i += 1
+    while iterable.__iter__:
+        if i <len(iterable):
+            yield (start, iterable[i])
+            start += 1
+            i += 1
 
 x = [1, 3, 5, 'a', '10', 3]
 
-for i in with_index2(x, 2):
+"""for i in with_index2(x, 2):
+    print(i)"""
+
+
+def with_index3(iterable, start = 0):
+
+    for element in iterable:
+
+        yield (start, element)
+        start += 1
+
+
+for i in with_index3(range(0, 10), 2):
     print(i)
