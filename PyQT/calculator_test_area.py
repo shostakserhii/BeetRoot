88bfd84e455aab8402c580d4cccf13fe6f2e0249
shostakserhii <62908234+shostakserhii@users.noreply.github.com>
2020-07-10 22:35:30 +0300

    first_try

--- PyQT/calculator_test_area.py
+++ PyQT/calculator_test_area.py
@@ -1,8 +1,10 @@
 import sys
 
 from functools import partial
-
+from operator import pow, truediv, mul, add, sub 
+from PyQt5.QtGui import QFont, QColor
 from PyQt5.QtCore import Qt
+
 from PyQt5.QtWidgets import (QApplication, 
                              QWidget,
                              QHBoxLayout,
@@ -12,7 +14,9 @@ from PyQt5.QtWidgets import (QApplication,
                              QPushButton,
                              QLineEdit,
                              QListWidget,
-                             QLabel)
+                             QLabel,
+                             QSizePolicy)
+
 
 
 class MainWindow(QMainWindow):
@@ -20,9 +24,16 @@ class MainWindow(QMainWindow):
         super().__init__(*args, **kwargs)
         self.setWindowTitle("Pyculator")
         self.setGeometry(100, 100, 280, 80)
-        self.first_value = 0
-        self.second_value = 0
-        self.operation = ""
+        self.first_value = ''
+        self.second_value = ''
+        self.operation = ''
+        self.result = ''
+        self.dot = 0
+        self.operations = {
+                           '+':add,
+                           '-':sub,
+                           '*':mul
+                           }
         self.widget = QWidget()
         self.mainLayout = QHBoxLayout()
         self.mainSubLayout = QVBoxLayout()
@@ -35,8 +46,6 @@ class MainWindow(QMainWindow):
         self.widget.setLayout(self.mainLayout)
         self.setCentralWidget(self.widget)
         self.buttons_processing()
-        self.input_processing()
-
 
     def _createDisplay(self):
         self.display = QLineEdit('')
@@ -150,6 +159,11 @@ class MainWindow(QMainWindow):
         for buttonConfig in buttons:
             name = buttonConfig.get('name','')
             btn = QPushButton(name)
+            font = QFont()
+            font.setBold(True)
+            btn.setFont(font)
+            btn.setSizePolicy(QSizePolicy.Preferred, 
+                              QSizePolicy.Expanding)
             self.buttons[name] = btn
             self.buttonLayout.addWidget(btn,
                                    buttonConfig['row'],
@@ -157,12 +171,9 @@ class MainWindow(QMainWindow):
                                    buttonConfig.get('rowSpan',1),
                                    buttonConfig.get('colSpan',1))
 
-        
+
     def change_text(self, text):
         self.display.setText(self.display.text() + text)
-
-    def input_value_processing(self):
-        pass
 
     def buttons_processing(self):
         for button_name in self.buttons:
@@ -170,51 +181,206 @@ class MainWindow(QMainWindow):
             if button_name in {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9'}:
                 btn.clicked.connect(partial(self.change_text, button_name))
             elif button_name == 'C':
-                btn.clicked.connect(self._clearDisplay)
+                btn.clicked.connect(self._clearAll)
             elif button_name == '←':
                 btn.clicked.connect(self._remove)
             elif button_name == '+':
                 btn.clicked.connect(self._additing)
+            elif button_name == '=':
+                btn.clicked.connect(self._ending)
+            elif button_name == '-':
+                btn.clicked.connect(self._substracting)
+            elif button_name == '*':
+                btn.clicked.connect(self._multiplication)
+            elif button_name == '.':
+                btn.clicked.connect(self._dotting)                
+
+    def _dotting(self):
+        value = self.displayText()
+        if '.' in value:
+            return
+        else:
+            self.display.setText(self.display.text() + ".")
+
+    def int_or_float(self, num):
+        try:
+            num = float(num)
+            if num.is_integer():
+                return int(num)
+            return num
+        except ValueError:
+            self._clearAll
+            self.history.addItem("ERROR")
+            return
 
     def input_processing(self):
         value = self.displayText()
+
+        if value == '':
+            return True
+
         print(f"value is {value}")
         print(f"first_value is {self.first_value}")
-        # if self.operation == '':
-        #     pass
-        if self.first_value == 0:
+        print(f"operation is {self.operation}")
+        print(f"second_value is {self.second_value}")
+        if self.first_value != '' and value == '':
+            return
+
+        if self.first_value == '':
             self.first_value = value
-            print(f"first_value is {self.first_value}")
-        else:
-            self.second_value = value
-            print(f"second_value is {self.second_value}")
-        print(f"operation {self.operation}")
+            print(f"1st IF first_value is {self.first_value}")
+            self.history.addItem(self.displayText())
+            self.history.addItem(self.operation)
+            self._clearDisplay()
+            return
 
-        
-        
+        elif self.first_value != '':
+            if self.second_value == '':
+                self.second_value = value
+                self.history.addItem(self.displayText())
+                self._clearDisplay()
+                print(f"ELIF first_value is {self.first_value}")
+                print(f"ELIF oeration is {self.operation}")
+                print(f"ELIF second_value is {self.second_value}")
+                return
+            elif self.second_value != '' and self.second_value != '':     
+                self.second_value = value
+                self.history.addItem(self.displayText())
+                self._clearDisplay()
+                print(f"ELSE elif second_value is {self.second_value}")
+                print(f"ELSE first_value is {self.first_value}")
+                print(f"ELSE oeration is {self.operation}")
+                print(f"ELSE second_value is {self.second_value}")
+                return
 
-    def setDisplayText(self, text):
-        self.display.setText('')
+    def setDisplayText(self, text=''):
+        self.display.setText(text)
         self.display.setFocus()
 
     def displayText(self):
         return self.display.text()
 
     def _clearDisplay(self):
-        self.setDisplayText('')
-    
+        self.setDisplayText()
+
+    def _clearAll(self):
+        self.setDisplayText()
+        self.first_value = ''
+        self.second_value = ''
+        self.result = ''
+        self.history.clear()
+
+    def validate(self, value):
+        if value.lstrip("-").replace('.','',1).isdigit() and value.count("-") <= 1:
+            return True
+
     def _remove(self):
         value = self.displayText()
+
         if len(value) == 0:
             self.setDisplayText("")
         else:
             value = value[:-1]
             self.display.setText(value)
-    
+
+    def _ending(self):
+        self.input_processing()
+
+        if self.second_value == '' and self.operation != '':
+            print('U are in ending now')
+            self.repeat = self.first_value
+            self.result = self.operations[self.operation](self.int_or_float(self.first_value), self.int_or_float(self.repeat))
+            self.result = ''
+            self.setDisplayText(str(self.first_value))
+            return
+
+        for symbol in self.operations:
+            if symbol == self.operation:
+                print(f"first value = {self.first_value}, second value = {self.second_value}")
+                self.result = self.operations[self.operation](self.int_or_float(self.first_value), self.int_or_float(self.second_value))
+
+        self.history.addItem('=')
+        self.history.addItem(str(self.int_or_float(self.result)))
+        self.first_value = self.result
+        self.result = ''
+        self.second_value = ''
+        # self.operation = ''
+        self.history.clear()
+        self.setDisplayText(str(self.first_value))
+        print(f"""after ending first value is {self.first_value}
+                                second value is {self.second_value}
+                                result is {self.result}
+        """)
+        self.first_value = ''
+
     def _additing(self):
         self.operation = '+'
-        self.input_processing()
-       
+        
+        if self.first_value == '':
+            self.input_processing()
+            return
+        elif self.first_value != '':    
+            if self.input_processing() is True:
+                self.history.addItem(self.operation)
+                return
+            else:
+                print(f"_additintg first value = {self.first_value} \nsecond_value is {self.second_value}")
+                left = self.int_or_float(self.first_value)
+                print(f"_additing left = {left}")
+                right = self.int_or_float(self.second_value)
+                print(f"_additing right = {right}")
+                self.result = left + right
+                self.history.addItem('=')
+                self.history.addItem(str(self.result))
+                self.history.addItem(self.operation)
+                self.first_value = self.result
+                return
+
+    def _substracting(self):
+        self.operation = '-'
+
+        if self.first_value == '':
+            self.input_processing()
+            return
+        elif self.first_value != '':    
+            if self.input_processing() is True:
+                self.history.addItem(self.operation)
+                return
+            else:
+                print(f"_additintg first value = {self.first_value} \nsecond_value is {self.second_value}")
+                left = self.int_or_float(self.first_value)
+                print(f"_additing left = {left}")
+                right = self.int_or_float(self.second_value)
+                print(f"_additing right = {right}")
+                self.result = left - right
+                self.history.addItem('=')
+                self.history.addItem(str(self.int_or_float(self.result)))
+                self.history.addItem(self.operation)
+                self.first_value = self.result
+                return
+
+    def _multiplication(self):
+        self.operation = '*'
+
+        if self.first_value == '':
+            self.input_processing()
+            return
+        elif self.first_value != '':    
+            if self.input_processing() is True:
+                self.history.addItem(self.operation)
+                return
+            else:
+                print(f"_additintg first value = {self.first_value} \nsecond_value is {self.second_value}")
+                left = self.int_or_float(self.first_value)
+                print(f"_additing left = {left}")
+                right = self.int_or_float(self.second_value)
+                print(f"_additing right = {right}")
+                self.result = left * right
+                self.history.addItem('=')
+                self.history.addItem(str(self.int_or_float(self.result)))
+                self.history.addItem(self.operation)
+                self.first_value = self.result
+                return
 
 
 def main_window():
@@ -222,8 +388,7 @@ def main_window():
     window = MainWindow()
     window.show()
     sys.exit(app.exec_())
-
     
 
 if __name__ == "__main__":
-    main_window()    
+    main_window() 
